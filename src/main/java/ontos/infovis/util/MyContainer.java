9286a36765044159ad4c7de904da81408647377e
Natalie Hube <nhube@stud.hs-bremen.de>
2015-06-04 14:44:58 +0200

    added class

--- src/main/java/ontos/infovis/util/MyContainer.java
+++ src/main/java/ontos/infovis/util/MyContainer.java
@@ -6,6 +6,14 @@ import java.util.List;
 import ontos.infovis.pojo.Component;
 import ontos.infovis.pojo.Composition;
 
+
+/**
+ *
+ * @author Natalie Hube
+ * @date 15-06-04
+ * @copyright infovis@tu-dresden.de
+ */
+
 public class MyContainer {
 
 	static public List<Component> myContainer =new ArrayList<Component>();
